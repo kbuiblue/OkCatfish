a11c5c742496ceb686ac2392df87630962d7b5ab
Khanh Bui <kbuiblue@gmail.com>
2023-08-02 18:18:00 +0700

    Initial commit

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+export const cats = [
+    {
+        name: "Nana",
+        avatar: "https://shorturl.at/mBYZ4",
+        age: 25,
+        bio: "If you don't swipe right, I'll cry 😢",
+        hasBeenSwiped: false,
+        hasBeenLiked: false,
+    },
+    {
+        name: "Beluga",
+        avatar: "https://shorturl.at/lDLST",
+        age: 19,
+        bio: "Yup, that's my owner. U can meet him if you want",
+        hasBeenSwiped: false,
+        hasBeenLiked: false,
+    },
+    {
+        name: "Smudge",
+        avatar: "https://shorturl.at/cew03",
+        age: 33,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false,
+    },
+];
