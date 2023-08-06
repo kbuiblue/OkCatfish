3939814da503ae61303b3f621cd44e34d0c2493a
Khanh Bui <kbuiblue@gmail.com>
2023-08-06 22:35:29 +0700

    Added like and nope images, swipe detection

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,12 @@
-function getAvatarHTML(numOfAvatars: number) {
-    if (numOfAvatars > 1) {
-        return `<div>
-            
-        </div>`
+export function getPagesHTML(numOfPages: number) {
+    let pageIndex = 0;
+
+    if (numOfPages > 1) {
+        return new Array(numOfPages)
+            .fill(0)
+            .map(() => `<div class ="page ${pageIndex === 0 ? "current__page" : ""}" data-index="${pageIndex++}"></div>`)
+            .join("");
+    } else {
+        return "";
     }
 }
